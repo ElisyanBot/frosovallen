b5c2780a0493539ac1352e5db1e2ce000f9accc8
Marcus Larsson <marcus.b.larsson@outlook.com>
2023-06-03 10:53:12 +0200

    added static homepage sections

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frosovallen/',
+  base: './',
   plugins: [vue()], 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      // '@components': path.resolve(__dirname, './src/components/'),
-      // '@features': path.resolve(__dirname, './src/features/'),
-      // '@layouts': path.resolve(__dirname, './src/layouts/'),
-      // '@assets': path.resolve(__dirname, './src/assets/'),
     },
   },
 })
